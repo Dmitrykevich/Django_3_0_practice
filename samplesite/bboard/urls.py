5556b5c3158eccc3378383149288a9e91c98dd78
Dmitry <dmitrykevich@yandex.ru>
2023-01-05 15:38:17 +0300

    chapter 8 sections 1 .. 7

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import index, by_rubric, BbCreateView
 
+app_name = 'bboard'
 urlpatterns = [
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
