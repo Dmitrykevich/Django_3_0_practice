f414462b75af1227cfd7799972a7d79bb4c55062
Dmitry <dmitrykevich@yandex.ru>
2023-01-13 17:25:12 +0300

    32.3.1. Bеб-страница правки основных данных пользователя

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, profile, BBLogoutView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView
 
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
