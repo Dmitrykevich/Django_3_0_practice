67ea903e8b08225e16c854f47c0e7654ec6cb9fa
Dmitry <dmitrykevich@yandex.ru>
2023-01-23 18:42:07 +0300

    36.1.4. Вывод и добавление комментариев

--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import bbs, BbDetailView
+from .views import bbs, BbDetailView, comments
 
 
 app_name = 'api'
 
 
 urlpatterns = [
+    path('bbs/<int:pk>/comments/', comments),
     path('bbs/<int:pk>/', BbDetailView.as_view()),
     path('bbs/', bbs),
 ]
